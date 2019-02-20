cb3ac639490f4017cb4df035ea91f0622c1f54b6
olympiawojcik <owojcik7@gmail.com>
2019-02-20 17:47:56 -0500

    Refactor App to render PostsPage component

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,49 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
-import dummyData from "./dummy-data";
-
 import SearchBar from "./components/SearchBar/SearchBar";
 import PostContainer from "./components/PostContainer/PostContainer";
+
+import PostsPage from "./components/PostContainer/PostsPage";
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      posts: [],
-      filteredPosts: []
-      //posts is an array of Post objects
-    };
+    this.state = {};
   }
-  componentDidMount() {
-    this.setState({
-      posts: dummyData //:characters
-    });
-  }
-
-  searchPosts = event => {
-    const posts = this.state.posts.filter(post => {
-      if (post.username.includes(event.target.value)) {
-        return post;
-      }
-    });
-    this.setState({ filteredPosts: posts });
-  };
 
   render() {
     return (
       <div className="app">
-        <div className="search-bar">
-          <SearchBar searchPosts={this.searchPosts} />
-        </div>
-        <div className="post-container">
-          <PostContainer
-            posts={
-              this.state.filteredPosts.length > 0
-                ? this.state.filteredPosts
-                : this.state.posts
-            }
-          />
-        </div>
+        <PostsPage />
       </div>
     );
   }
---
+++ instagram/src/components/PostContainer/PostsPage.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+
+import SearchBar from "../SearchBar/SearchBar";
+
+import PostContainer from "./PostContainer";
+import dummyData from "./dummy-data";
+
+class PostsPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: [],
+      filteredPosts: []
+      //posts is an array of Post objects
+    };
+  }
+  componentDidMount() {
+    this.setState({
+      posts: dummyData //:characters
+    });
+  }
+
+  searchPosts = event => {
+    const posts = this.state.posts.filter(post => {
+      if (post.username.includes(event.target.value)) {
+        return post;
+      }
+    });
+    this.setState({ filteredPosts: posts });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <div className="search-bar">
+          <SearchBar searchPosts={this.searchPosts} />
+        </div>
+        <div className="post-container">
+          <PostContainer
+            posts={
+              this.state.filteredPosts.length > 0
+                ? this.state.filteredPosts
+                : this.state.posts
+            }
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PostsPage;
