8a0393c7ece34f7270f0dddecd31ea5908587e52
olympiawojcik <owojcik7@gmail.com>
2019-02-21 20:03:36 -0500

    Add instagram mobile photo to Login page and margin on App

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -2,20 +2,31 @@ import React from "react";
 // import Form from "react-bootstrap/Form";
 // import Button from "react-bootstrap/Button";
 import IGLogo from "../../assets/iglogo.png";
+import IGMobile from "../../assets/instagram-mobile.png";
 
 //import styled components and create a styled button, use it to style the entire page
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const Wrapper = styled.div`
   margin: 100px auto;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   width: 350px;
   height: 355px;
   border: 1px solid lightgrey;
   justify-content: center;
   align-items: center;
   align-content: space-around;
+  background-color: white;
+
+  ${props =>
+    props.primary &&
+    css`
+      display: flex;
+      flex-direction: column;
+      width: 350px;
+      height: 354px;
+    `}
 `;
 
 const Button = styled.button`
@@ -30,22 +41,39 @@ const Button = styled.button`
 `;
 
 const Image = styled.img`
-  width: 175px;
-  height: 50px;
+  width: 200px;
+  height: auto;
+  margin-bottom: 20px;
+
+  ${props =>
+    props.primary &&
+    css`
+      width: 370px;
+      height: auto;
+    `}
+`;
+
+const Input = styled.input`
+  width: 266px;
 `;
 
 const Login = props => {
   return (
-    <Wrapper>
-      <Image src={IGLogo} alt="Instagram Logo" />
-      <input
-        type="text"
-        value={props.inputText}
-        onChange={props.handleChanges}
-        placeholder="username@gmail.com"
-      />
-      <Button onClick={props.signIn}>Log In</Button>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <Image src={IGMobile} alt="Instagram mobile logo" primary />
+        <Wrapper primary>
+          <Image src={IGLogo} alt="Instagram Logo" />
+          <Input
+            type="text"
+            value={props.inputText}
+            onChange={props.handleChanges}
+            placeholder=" Username"
+          />
+          <Button onClick={props.signIn}>Log In</Button>
+        </Wrapper>
+      </Wrapper>
+    </>
   );
 };
 
