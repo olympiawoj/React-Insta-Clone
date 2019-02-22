2f7dd8b7b2f5e2edde0d8d0a5f98a423612599e1
olympiawojcik <owojcik7@gmail.com>
2019-02-21 19:01:53 -0500

    Add styling to Login component

--- instagram/src/components/Login/Login.js
+++ instagram/src/components/Login/Login.js
@@ -1,19 +1,50 @@
 import React from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+// import Form from "react-bootstrap/Form";
+// import Button from "react-bootstrap/Button";
 import IGLogo from "../../assets/iglogo.png";
+
+//import styled components and create a styled button, use it to style the entire page
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  margin: 100px auto;
+  display: flex;
+  flex-direction: column;
+  width: 350px;
+  height: 355px;
+  border: 1px solid lightgrey;
+  justify-content: center;
+  align-items: center;
+  align-content: space-around;
+`;
+
+const Button = styled.button`
+  background: #308cee;
+  border-radius: 3px;
+  color: white;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  font-weight: bold;
+  width: 266px;
+  margin-top: 15px;
+`;
+
+const Image = styled.img`
+  width: 175px;
+  height: 50px;
+`;
 
 const Login = props => {
   return (
-    <>
-      <img src={IGLogo} alt="Instagram Logo" />
+    <Wrapper>
+      <Image src={IGLogo} alt="Instagram Logo" />
       <input
         type="text"
         value={props.inputText}
         onChange={props.handleChanges}
       />
-      <button onClick={props.signIn}>Sign In</button>
-    </>
+      <Button onClick={props.signIn}>Log In</Button>
+    </Wrapper>
   );
 };
 
